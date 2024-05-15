a0047aaa372a28c6fe58cbaa1c240cb173f1094b
Abner Zheng <abnerzheng@gmail.com>
2024-05-15 22:42:33 +0800

    finish w1/d4, Task 1: SST Builder

--- mini-lsm-starter/src/table.rs
+++ mini-lsm-starter/src/table.rs
@@ -6,13 +6,15 @@ mod builder;
 mod iterator;
 
 use std::fs::File;
+use std::mem;
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
 pub use builder::SsTableBuilder;
-use bytes::Buf;
+use bytes::{Buf, BufMut};
 pub use iterator::SsTableIterator;
+use log::warn;
 
 use crate::block::Block;
 use crate::key::{KeyBytes, KeySlice};
@@ -30,21 +32,42 @@ pub struct BlockMeta {
     pub last_key: KeyBytes,
 }
 
+const SIZE_OF_USIZE: usize = mem::size_of::<usize>();
+
 impl BlockMeta {
     /// Encode block meta to a buffer.
     /// You may add extra fields to the buffer,
     /// in order to help keep track of `first_key` when decoding from the same buffer in the future.
-    pub fn encode_block_meta(
-        block_meta: &[BlockMeta],
-        #[allow(clippy::ptr_arg)] // remove this allow after you finish
-        buf: &mut Vec<u8>,
-    ) {
-        unimplemented!()
+    pub fn encode_block_meta(block_meta: &[BlockMeta], buf: &mut Vec<u8>) {
+        let meta_offset = buf.len() as u32;
+        for meta in block_meta {
+            buf.put_u32(meta.offset as u32);
+            buf.put_u16(meta.first_key.len() as u16);
+            buf.put(meta.first_key.raw_ref());
+            buf.put_u16(meta.last_key.len() as u16);
+            buf.put(meta.last_key.raw_ref());
+        }
+        buf.put_u32(meta_offset);
     }
 
     /// Decode block meta from a buffer.
-    pub fn decode_block_meta(buf: impl Buf) -> Vec<BlockMeta> {
-        unimplemented!()
+    pub fn decode_block_meta(mut buf: impl Buf) -> Vec<BlockMeta> {
+        let mut res = vec![];
+        while buf.has_remaining() {
+            let offset = buf.get_u32() as usize;
+            let first_key_len = buf.get_u16();
+            let first_key = KeyBytes::from_bytes(buf.copy_to_bytes(first_key_len as usize));
+            let last_key_len = buf.get_u16();
+            let last_key = KeyBytes::from_bytes(buf.copy_to_bytes(last_key_len as usize));
+
+            res.push(BlockMeta {
+                offset,
+                first_key,
+                last_key,
+            })
+        }
+
+        res
     }
 }
 
--- mini-lsm-starter/src/table/builder.rs
+++ mini-lsm-starter/src/table/builder.rs
@@ -1,19 +1,22 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use std::mem;
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Result;
+use nom::ExtendInto;
 
-use super::{BlockMeta, SsTable};
+use super::{BlockMeta, FileObject, SsTable};
+use crate::key::{KeyBytes, KeyVec};
 use crate::{block::BlockBuilder, key::KeySlice, lsm_storage::BlockCache};
 
 /// Builds an SSTable from key-value pairs.
 pub struct SsTableBuilder {
     builder: BlockBuilder,
-    first_key: Vec<u8>,
-    last_key: Vec<u8>,
+    first_key: KeyVec,
+    last_key: KeyVec,
     data: Vec<u8>,
     pub(crate) meta: Vec<BlockMeta>,
     block_size: usize,
@@ -22,7 +25,14 @@ pub struct SsTableBuilder {
 impl SsTableBuilder {
     /// Create a builder based on target block size.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            builder: BlockBuilder::new(block_size),
+            first_key: KeyVec::default(),
+            last_key: KeyVec::default(),
+            data: vec![],
+            meta: vec![],
+            block_size,
+        }
     }
 
     /// Adds a key-value pair to SSTable.
@@ -30,7 +40,32 @@ impl SsTableBuilder {
     /// Note: You should split a new block when the current block is full.(`std::mem::replace` may
     /// be helpful here)
     pub fn add(&mut self, key: KeySlice, value: &[u8]) {
-        unimplemented!()
+        if self.first_key.is_empty() {
+            self.first_key.set_from_slice(key);
+        }
+
+        let added = self.builder.add(key, value);
+        if !added {
+            // split a new block
+            let mut new_builder = BlockBuilder::new(self.block_size);
+            let added = new_builder.add(key, value);
+            assert!(added, "Not able to add key pair to a new created block");
+
+            let old_builder = mem::replace(&mut self.builder, new_builder);
+            let block = old_builder.build().encode();
+            let old_first_key = mem::replace(&mut self.first_key, key.to_key_vec());
+            let old_last_key = mem::replace(&mut self.last_key, key.to_key_vec());
+
+            self.meta.push(BlockMeta {
+                offset: self.data.len(),
+                first_key: old_first_key.into_key_bytes(),
+                last_key: old_last_key.into_key_bytes(),
+            });
+
+            self.data.extend_from_slice(&*block);
+        } else {
+            self.last_key.set_from_slice(key);
+        }
     }
 
     /// Get the estimated size of the SSTable.
@@ -38,17 +73,47 @@ impl SsTableBuilder {
     /// Since the data blocks contain much more data than meta blocks, just return the size of data
     /// blocks here.
     pub fn estimated_size(&self) -> usize {
-        unimplemented!()
+        self.data.len()
     }
 
     /// Builds the SSTable and writes it to the given path. Use the `FileObject` structure to manipulate the disk objects.
     pub fn build(
-        self,
+        mut self,
         id: usize,
         block_cache: Option<Arc<BlockCache>>,
         path: impl AsRef<Path>,
     ) -> Result<SsTable> {
-        unimplemented!()
+        // seal the currently active block
+        let block = self.builder.build().encode();
+        let old_first_key = mem::take(&mut self.first_key);
+        let old_last_key = mem::take(&mut self.last_key);
+
+        self.meta.push(BlockMeta {
+            offset: self.data.len(),
+            first_key: old_first_key.into_key_bytes(),
+            last_key: old_last_key.into_key_bytes(),
+        });
+        self.data.extend_from_slice(&block);
+
+        Ok(SsTable {
+            block_meta_offset: self.data.len(),
+            file: FileObject::create(path.as_ref(), self.data)?,
+            id,
+            block_cache,
+            first_key: self
+                .meta
+                .first()
+                .map(|m| m.first_key.clone())
+                .unwrap_or_default(),
+            last_key: self
+                .meta
+                .last()
+                .map(|m| m.last_key.clone())
+                .unwrap_or_default(),
+            block_meta: self.meta,
+            bloom: None,
+            max_ts: 0,
+        })
     }
 
     #[cfg(test)]
