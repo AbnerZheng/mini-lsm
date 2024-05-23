dc145bf4e95d9e6f1b3b55eb34bb828f717c1477
Abner Zheng <abnerzheng@gmail.com>
2024-05-23 16:45:50 +0800

    finish w2/d5, Task 1: Manifest Encoding

--- mini-lsm-starter/src/manifest.rs
+++ mini-lsm-starter/src/manifest.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)] // REMOVE THIS LINE after fully implementing this functionality
 
+use std::fs;
 use std::fs::File;
+use std::io::{Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
@@ -22,12 +24,27 @@ pub enum ManifestRecord {
 }
 
 impl Manifest {
-    pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        let file = fs::OpenOptions::new().create(true).write(true).open(path)?;
+        Ok(Self {
+            file: Arc::new(Mutex::new(file)),
+        })
     }
 
-    pub fn recover(_path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
-        unimplemented!()
+    pub fn recover(path: impl AsRef<Path>) -> Result<(Self, Vec<ManifestRecord>)> {
+        let mut file = fs::OpenOptions::new().read(true).append(true).open(path)?;
+        let mut buf = vec![];
+        file.read_to_end(&mut buf)?;
+        let records = serde_json::from_slice::<ManifestRecord>(&buf)
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        Ok((
+            Manifest {
+                file: Arc::new(Mutex::new(file)),
+            },
+            records,
+        ))
     }
 
     pub fn add_record(
@@ -38,7 +55,11 @@ impl Manifest {
         self.add_record_when_init(record)
     }
 
-    pub fn add_record_when_init(&self, _record: ManifestRecord) -> Result<()> {
-        unimplemented!()
+    pub fn add_record_when_init(&self, record: ManifestRecord) -> Result<()> {
+        let bytes = serde_json::to_vec(&record)?;
+        let mut guard = self.file.lock();
+        guard.write_all(bytes.as_slice())?;
+        guard.flush()?;
+        Ok(())
     }
 }
