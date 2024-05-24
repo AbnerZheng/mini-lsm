0565592825e132b20459068d4a05c853dc01a53c
Abner Zheng <abnerzheng@gmail.com>
2024-05-24 12:17:04 +0800

    finish w2/d7, task2: Block checksum

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -7,6 +7,7 @@ use bytes::{Buf, BufMut, Bytes};
 pub use iterator::BlockIterator;
 
 pub(crate) const SIZEOF_U16: usize = std::mem::size_of::<u16>();
+pub(crate) const SIZEOF_U32: usize = std::mem::size_of::<u32>();
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
 #[derive(Default)]
@@ -24,16 +25,22 @@ impl Block {
             res.put_u16(*x);
         }
         res.put_u16(self.offsets.len() as u16);
+        let crc = crc32fast::hash(&res);
+        res.put_u32(crc);
         Bytes::from(res)
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
-    /// --------------------------------------------------------------------------------------------------------
-    /// |             Data Section             |              Offset Section                  |      Extra      |
-    /// ---------------------------------------------------------------------------------------------------------
-    /// | Entry #1 | Entry #2 | ... | Entry #N | Offset #1(u16) | Offset #2 | ... | Offset #N | num_of_elements |
-    /// ---------------------------------------------------------------------------------------------------------
+    /// --------------------------------------------------------------------------------------------------------------------
+    /// |             Data Section             |              Offset Section                  |      Extra      | checcksum |
+    /// --------------------------------------------------------------------------------------------------------------------
+    /// | Entry #1 | Entry #2 | ... | Entry #N | Offset #1(u16) | Offset #2 | ... | Offset #N | num_of_elements |  u32     |
+    /// -------------------------------------------------------------------------------------------------------------------
     pub fn decode(data: &[u8]) -> Self {
+        let (data, mut crc_raw) = data.split_at(data.len() - SIZEOF_U32);
+        let crc = crc_raw.get_u32();
+        let crc_actual = crc32fast::hash(data);
+        assert_eq!(crc, crc_actual, "corrupted block");
         let num_of_elements = (&data[data.len() - SIZEOF_U16..]).get_u16() as usize;
 
         let offset = data[data.len() - SIZEOF_U16 * (num_of_elements + 1)..data.len() - SIZEOF_U16]
