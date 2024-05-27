785c985a680b34075360fd1c75cda0fca1bb9eee
Abner Zheng <abnerzheng@gmail.com>
2024-05-27 13:56:09 +0800

    finish w2/d4: Task 1.2: Decide Base Level

--- mini-lsm-starter/src/compact/leveled.rs
+++ mini-lsm-starter/src/compact/leveled.rs
@@ -1,4 +1,6 @@
+use log::warn;
 use serde::{Deserialize, Serialize};
+use std::process::id;
 
 use crate::lsm_storage::LsmStorageState;
 
@@ -40,7 +42,7 @@ impl LeveledCompactionController {
 
     pub fn target_size(&self, max_level_size_mb: usize) -> Vec<usize> {
         let mut res = vec![0; self.options.max_levels];
-        let mut cur = max_level_size_mb;
+        let mut cur = max_level_size_mb.max(self.options.base_level_size_mb);
         for i in (0..self.options.max_levels).rev() {
             res[i] = cur;
             if cur <= self.options.base_level_size_mb {
@@ -57,28 +59,66 @@ impl LeveledCompactionController {
         snapshot: &LsmStorageState,
     ) -> Option<LeveledCompactionTask> {
         assert_eq!(snapshot.levels.len(), self.options.max_levels);
-        if snapshot.l0_sstables.len() > self.options.level0_file_num_compaction_trigger {
-            // use number of file as file size to simplify problem
-            let target_size =
-                self.target_size(snapshot.levels[self.options.max_levels - 1].1.len());
-            let idx = target_size.iter().position(|&x| x > 0);
+        if snapshot.l0_sstables.len() >= self.options.level0_file_num_compaction_trigger {
+            let max_level_size = snapshot.levels[self.options.max_levels - 1]
+                .1
+                .iter()
+                .map(|sst_id| snapshot.sstables[sst_id].file.size())
+                .sum::<u64>()
+                / 1024
+                / 1024;
+            let target_size = self.target_size(max_level_size as usize);
+            let idx = target_size
+                .iter()
+                .position(|&x| x > 0)
+                .expect("target size should have at least 1 positive value");
+
+            println!("flush L0 SST to base level {}", idx + 1);
             return Some(LeveledCompactionTask {
                 upper_level: None,
-                upper_level_sst_ids: vec![],
-                lower_level: 0,
-                lower_level_sst_ids: vec![],
+                upper_level_sst_ids: snapshot.l0_sstables.clone(),
+                lower_level: idx,
+                lower_level_sst_ids: snapshot.levels[idx].1.clone(),
                 is_lower_level_bottom_level: false,
             });
         }
-        unimplemented!()
+        None
     }
 
     pub fn apply_compaction_result(
         &self,
-        _snapshot: &LsmStorageState,
-        _task: &LeveledCompactionTask,
-        _output: &[usize],
+        snapshot: &LsmStorageState,
+        task: &LeveledCompactionTask,
+        output: &[usize],
     ) -> (LsmStorageState, Vec<usize>) {
-        unimplemented!()
+        let mut snapshot = snapshot.clone();
+        let LeveledCompactionTask {
+            upper_level,
+            upper_level_sst_ids,
+            lower_level,
+            lower_level_sst_ids,
+            is_lower_level_bottom_level,
+        } = task;
+        let mut files_to_remove = vec![];
+        match upper_level {
+            None => {
+                // compact l0
+                assert_eq!(
+                    snapshot.l0_sstables, *upper_level_sst_ids,
+                    "state change during compaction"
+                );
+                snapshot.l0_sstables = vec![];
+                files_to_remove.extend_from_slice(upper_level_sst_ids);
+                let (level_id, sst_ids) = &snapshot.levels[*lower_level];
+                assert_eq!(
+                    *sst_ids, *lower_level_sst_ids,
+                    "state change during compaction"
+                );
+                files_to_remove.extend_from_slice(sst_ids);
+                snapshot.levels[*lower_level] = (*level_id, output.to_vec());
+            }
+            Some(_) => {}
+        }
+        (snapshot, files_to_remove)
     }
 }
